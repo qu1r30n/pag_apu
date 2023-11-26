2880e8978ea685781d23d5685030a65157d2d463
qu1r30n <qu1r30n@hotmail.com>
2023-11-26 16:48:32 -0600

    todas las imagenes de las cartas

---
+++ proseso.js
@@ -0,0 +1,15 @@
+// script.js
+
+function mostrarImagen() {
+  var nombre = document.getElementById('imagenNombre').value;
+  var carpeta = 'imagenes/todas/';
+  var url = carpeta + nombre;
+  var resultado = document.getElementById('resultado');
+
+  // Verificar si el campo de nombre está lleno y la URL es válida
+  if (nombre && url.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+    resultado.innerHTML = '<img id="output" src="' + url + '" alt="' + nombre + '">';
+  } else {
+    resultado.innerHTML = '<p>Por favor, ingresa un nombre válido.</p>';
+  }
+}
