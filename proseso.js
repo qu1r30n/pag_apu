90a70c3cd305b86e38f4dd1231fed9eb5dab8ad3
qu1r30n <qu1r30n@hotmail.com>
2023-11-26 19:44:04 -0600

    todo el codigo comentado

--- proseso.js
+++ proseso.js
@@ -1,15 +1,18 @@
+// Evento que se activa al seleccionar imágenes
 document.getElementById('imagenes').addEventListener('change', function (event) {
     var previewDiv = document.getElementById('preview');
     previewDiv.innerHTML = ''; // Limpiar la vista previa antes de agregar nuevas imágenes
 
     var files = event.target.files;
 
+    // Iterar sobre los archivos seleccionados
     for (var i = 0; i < files.length; i++) {
         (function (file) {
             // Validar que el archivo sea una imagen
             if (file.type.startsWith('image/')) {
                 var reader = new FileReader();
 
+                // Cuando la imagen se carga, agregarla a la vista previa
                 reader.onload = function (e) {
                     // Crear un elemento de imagen y establecer su fuente
                     var img = document.createElement('img');
@@ -31,6 +34,7 @@ document.getElementById('imagenes').addEventListener('change', function (event) 
     }
 });
 
+// Función para buscar imágenes por ID
 function buscarImagen(event) {
     if (event.key === 'Enter') {
         var buscarImagenInput = document.getElementById('buscarImagen');
@@ -38,12 +42,14 @@ function buscarImagen(event) {
 
         var imagenes = document.getElementById('preview').getElementsByTagName('img');
 
+        // Iterar sobre las imágenes y aplicar el filtro
         for (var i = 0; i < imagenes.length; i++) {
             var img = imagenes[i];
 
             // Convertir tanto el ID de la imagen como el filtro a minúsculas para una comparación insensible a mayúsculas y minúsculas
             var imgId = img.id.toLowerCase();
 
+            // Mostrar u ocultar la imagen según si coincide con el filtro
             if (imgId.includes(filtro)) {
                 img.style.display = 'inline-block';
             } else {
